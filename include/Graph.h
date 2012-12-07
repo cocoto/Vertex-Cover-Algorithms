7c87bfca339f04d6130b4e481ebf70c40b8fa1f6
cocoto <qtonneau@gmail.com>
2012-12-07 14:25:54 +0100

    update todo Graph Class

--- include/Graph.h
+++ include/Graph.h
@@ -11,13 +11,26 @@
 
 #include "Couple.h"
 
-class Couple;
-
+/*
+ * TODO : Ajouter la définition des classes
+ * 
+ * std::map<int, std::list<int> > *edges = new std::map<int, std::list<int> >;
+ * std::set<Couple> *nodes = new std::set<Couple>;
+ * 
+ * Dans la classe (private)
+ *
+ * TODO : Définir les accesseurs
+ * TODO : Définir l'opérateur [] = edges.[]
+ * TODO : Définir la fonction erase_edge(const int &node1, const int &node2) = efface les deux arrètes (2--1, 1--2)
+ * TODO : Définir la fonction erase_node(const int &) = supression de nodes, supression des arretes adjacentes
+ * TODO : Définir la fonction bool empty_edge() = toutes les arrêtes sont couvertes
+ */
 class Graph
 {
 	private :
 		int _max, _edges, _nodes;
 		double _avg;
+		
 	public :
 		Graph(int, double, std::map<int, std::list<int> >*&
 			, std::set<Couple> *&nodes);
