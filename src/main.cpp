4d7771a7b6c33182d608ac159afb12b6412ec9f4
cocoto <qtonneau@gmail.com>
2012-12-12 20:41:17 +0100

    update main -> test de KernelVC et 2-approx

--- src/main.cpp
+++ src/main.cpp
@@ -24,5 +24,18 @@ int main(int argc, char **argv)
 	std::cout << graph;
 	std::list<int> d_approx=fonction2approx(graph);
 	std::cout<<d_approx.size()<<"\n";
+	std::list<int>::iterator it=d_approx.begin();
+	while(it!=d_approx.end())
+	{
+	  std::cout<<*it<<" ";
+	  it++;
+	}
+	std::cout<<"\n";
+	int i;
+ 	for(i=0;i<=(int)d_approx.size();i++)
+	{
+	  std::cout<<i<<" = "<<(fonctionKernelVC(graph, i)?"true":"false")<<"\n";
+	}
+	
 	return 1;
 }
