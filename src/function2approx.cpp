cd75f623cccf4ec83ce02c0ca456d1fbe495b714
cocoto <qtonneau@gmail.com>
2012-12-20 17:18:44 +0100

    add exceptions resolv

--- src/function2approx.cpp
+++ src/function2approx.cpp
@@ -11,10 +11,15 @@ int function2approx(Graph graphe)
     sommet1=graphe.id(); //selection du sommet de degré le plus grand (u)
     sommet2=graphe[sommet1]->front(); //selection d'un sommet lié (v)
     resultat+=2;
+    try{
     //Suppression de u (et de ses arrêtes)
     graphe.erase_node(sommet1);
     //Suppression de v (et de ses arrêtes)
     graphe.erase_node(sommet2);
+    }
+    catch(std::string s)
+    {
+    }
   }
 return resultat;
 }
